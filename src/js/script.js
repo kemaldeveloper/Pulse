a902de87569ecc6b5f9e29fc7d5ff62dd71048e9
Kemal <gurbansahadowkemal01@gmail.com>
2022-09-26 16:34:26 +0300

    add styles

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    speed: 1200,
+    adaptiveHeight: true,
+    prevArrow: '<button type="Button" class="slick-prev"><img src="..icons/left.svg"></button>',
+    nextArrow: '<button type="Button" class="slick-next">img src="..icons/right.svg"></button>',
+  })
+})
