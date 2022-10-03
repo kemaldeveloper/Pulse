15b3268256bb79baece08e551876689a7e61cf48
Kemal <gurbansahadowkemal01@gmail.com>
2022-10-03 15:44:11 +0300

    add scripts, files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync')
+const babel = require('gulp-babel')
 const sass = require('sass')
+const uglify = require('gulp-uglify')
 const gulpSass = require('gulp-sass')(sass)
 const rename = require('gulp-rename')
 const autoprefixer = require('gulp-autoprefixer')
@@ -13,6 +15,20 @@ gulp.task('server', function () {
       baseDir: 'src',
     },
   })
+})
+
+gulp.task('scripts', function () {
+  return gulp
+    .src('src/js/script.js')
+    .pipe(
+      babel({
+        presets: ['@babel/env'],
+      }),
+    )
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('src/js'))
+    .pipe(browserSync.stream())
 })
 
 gulp.task('styles', function () {
@@ -34,6 +50,7 @@ gulp.task('styles', function () {
 gulp.task('watch', function () {
   gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'))
   gulp.watch('src/*.html').on('change', browserSync.reload)
+  gulp.watch('src/js/script.js').on('change', gulp.parallel('scripts'))
 })
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts'))
